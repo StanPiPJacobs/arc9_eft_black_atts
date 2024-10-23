e1e00ba62aab9f252963ad08cd8af7bb36064727
Stan Jacobs <83178700+StanPiPJacobs@users.noreply.github.com>
2024-10-23 11:32:29 +0500

    camo grip for antifur camo

--- lua/arc9/common/attachments_bulk/eft_cst_black_atts_bulk.lua
+++ lua/arc9/common/attachments_bulk/eft_cst_black_atts_bulk.lua
@@ -138,6 +138,41 @@ ATT.ModelMaterial = "models/weapons/arc9/darsu_eft/mods/pistolgrip_ar15_f1_firea
 
 ARC9.LoadAttachment(ATT, "eft_ar_pgrip_f1s2pc_wht")
 
+///////////////////////////////////////      eft_ar_pgrip_f1s2pc_camo
+
+ATT = {}
+
+ATT.PrintName = "AR-15 F1 Firearms Skeletonized Style 2 PC pistol grip"
+ATT.CompactName = "F1 St2 PC"
+ATT.Description = [[A lightweight ergonomical pistol grip with finger grooves for AR-15 weapon systems, manufactured by F1 Firearms. Wrapped with paracord for maximum comfort and minimum hand slip.]]
+ATT.Icon = Material("entities/eft_attachments/pgrips/ar15grips/f1s2pc_camo.png", "mips smooth")
+
+ATT.Model = "models/weapons/arc9/darsu_eft/mods/pistolgrip_ar15_f1_firearms_st2_pc_skeletonized.mdl"
+
+ATT.HasGrip = true
+
+ATT.EFTErgoAdd = 13
+ATT.CustomPros = { Ergonomics = "+13" }
+
+ATT.SortOrder = 0
+ATT.Category = "eft_ar15_pgrip"
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+
+ATT.Attachments = {
+    {
+        PrintName = "Camo",
+        Category = {"universal_camo"},
+        InstalledElements = {"camos"},
+        ExcludeElements = {"skins"},
+        Pos = Vector(1.75, 0, 2.5),
+        Ang = Angle(0,0,0),
+    },
+}
+
+ATT.ModelMaterial = "models/weapons/arc9/darsu_eft/mods/pistolgrip_ar15_f1_firearms_st2_pc_skeletonized_CAMO"
+
+ARC9.LoadAttachment(ATT, "eft_ar_pgrip_f1s2pc_camo")
+
 
 ///////////////////////////////////////      eft_ar_pgrip_hkv2_blk
 
